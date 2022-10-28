77a953c75ce7bc14f3b3ac845afd6b7e8caa6a13
naorasms <naorasms@gmail.com>
2022-10-28 21:08:30 +0300

    work on more page order

--- project/js/main.js
+++ project/js/main.js
@@ -1,1 +1,17 @@
-console.log("heloooooooooo");
+let tabs = document.querySelectorAll('[data-tab-target]');
+let tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        let target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')       
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
+console.log("sssssssssssssss")
