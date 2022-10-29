5fb63982f6b0f6c72150d36a8adbd5948a1a6ad2
naorasms <naorasms@gmail.com>
2022-10-29 19:10:48 +0300

    work on review product with javascript has been finshed

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,6 +7,11 @@
 let tabs = document.querySelectorAll('[data-tab-target]');
 let tabContents = document.querySelectorAll('[data-tab-content]')
 
+//popup modal
+let openPopup = document.querySelectorAll('[data-popup-target]');
+let closePopup = document.querySelectorAll('[data-close-button]');
+let overlay = document.getElementById('overlay')
+
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         let target = document.querySelector(tab.dataset.tabTarget)
@@ -21,3 +26,38 @@ tabs.forEach(tab => {
     })
 })
 console.log("sssssssssssssss")
+
+openPopup.forEach(a => {
+    a.addEventListener('click', () =>{
+        let target = document.querySelector(a.dataset.popupTarget)
+        console.log(target)
+        openModel(target)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    let modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal =>{
+        closeModel(modal)
+    })
+})
+
+closePopup.forEach(a => {
+    a.addEventListener('click', () =>{
+        let target = a.closest('.modal')
+        closeModel(target)
+    })
+})
+
+
+function openModel (target) {
+    if(target == null) return
+    target.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModel (target) {
+    if(target == null) return
+    target.classList.remove('active')
+    overlay.classList.remove('active')
+}
