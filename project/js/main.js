9c05a491458816a63ad014365da3b17d2827059e
naorasms <naorasms@gmail.com>
2022-11-07 21:02:53 +0300

    Complete all modifications

--- project/js/main.js
+++ project/js/main.js
@@ -9,15 +9,23 @@ let overlay = document.getElementById('overlay');
 // Menu Mobil
 let sidebar = document.querySelector(".sidebar");
 let menu_mobil = document.querySelector(".menu_mobile");
+let menu_icon = document.querySelector(".menu");
 
 
 if(menu_mobil){
 
     menu_mobil.addEventListener('click', () => {
-        sidebar.classList.toggle("active")
+        sidebar.classList.toggle("active");
+        menu_mobil.classList.toggle("show")
+        console.log(menu_icon)
     })
 }
-console.log(menu_mobil)
+
+// menu_icon.addEventListener('click', () => {
+//     menu_icon.classList.toggle("active")
+//     console.log(menu_icon)
+// })
+
 tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         let target = document.querySelector(tab.dataset.tabTarget)
@@ -31,7 +39,7 @@ tabs.forEach(tab => {
         target.classList.add('active')
     })
 })
-console.log("sssssssssssssss")
+
 
 openPopup.forEach(btn => {
     btn.addEventListener('click', () =>{
@@ -69,160 +77,28 @@ function closeModel (target) {
     overlay.classList.remove('active')
 }
 
-
-// swaper slider
-// var swiper = new Swiper(".swiper", {
-//     slidesPerView: 3,
-//     spaceBetween: 20,
-//     // slidesPerGroup: 3,
-//     loop: true,
-//     width: 300,
-//     // loopFillGroupWithBlank: true,
-//     // pagination: {
-//     //   el: ".swiper-pagination",
-//     //   clickable: true,
-//     // },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
-
-// const swiper = new Swiper('.swiper', {
-//     // Default parameters
-//     slidesPerView: 1,
-//     spaceBetween: 10,
-//     width: 100,
-//     loop: true,
-//     // Responsive breakpoints
-//     container: {
-//       // when window width is >= 320px
-//       320: {
-//         slidesPerView: 2,
-//         spaceBetween: 20
-//       },
-//       // when window width is >= 480px
-//       480: {
-//         slidesPerView: 3,
-//         spaceBetween: 30
-//       },
-//       // when window width is >= 640px
-//       640: {
-//         slidesPerView: 4,
-//         spaceBetween: 40
-//       }
-      
-//     }
-//   })
-
-
-
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 15,
-//     width: 100,
-//     loop: true,
-//     centerSlider: true,
-//     grabCursor: 'true',
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-              
-//               breakpoints: {
-//                 640: {
-//                     slidesPerView: 2,
-//                     spaceBetween: 20,
-//                 },
-//                 768: {
-//                     slidesPerView: 3,
-//                     spaceBetween: 0,
-//                 },
-//                 1024: {
-//                     slidesPerView: 4,
-//                     spaceBetween: 0,
-//                 },
-//             },
-
-
-//   });
-
-
-// new Splide( '.splide' ).mount();
-
-// var splide = new Splide( '.splide', {
-//     type     : 'loop',
-//     height   : '10rem',
-//     focus    : 'center',
-//     container: HTMLElement,
-//     autoWidth: true,
-//   } );
-  
-//   splide.mount();
-
-
-// var splide = new Splide( '.splide', {
-//     perPage: 6,
-//     type   : 'loop',
-//     gap    : '2rem',
-//     // fixedWidth:  "100px",
-//     breakpoints: {
-//       640: {
-//         perPage: 4,
-//         gap    : '.7rem',
-//         // height : '6rem',
-//       },
-//       480: {
-//         perPage: 3,
-//         gap    : '.7rem',
-//         // pagination: true
-//         // height : '6rem',
-//       },
-//     },
-//   } );
-  
-//   splide.mount();
-
-
-
-
-// var splide = new Splide( '.splide', {
-//     perPage: 5,
-//     rewind : true,
-//     type   : 'loop',
-//     gap    : '2rem',
-//         breakpoints: {
-//       640: {
-//         perPage: 3,
-//         gap    : '.7rem',
-//         // height : '6rem',
-//       },
-//       480: {
-//         perPage: 2,
-//         gap    : '.7rem',
-//         // pagination: true
-//         // height : '6rem',
-//       },
-//     },
-//   } );
+// spalide slider
 var elms = document.getElementsByClassName( 'splide' );
 
 for ( var i = 0; i < elms.length; i++ ) {
   new Splide( elms[ i ],
         {
-
-            perPage: 5,
+            perPage: 6,
             rewind : true,
-            type   : 'loop',
-            // gap    : '2rem',
+            // type   : 'loop',
+            gap    : '2rem',
                 breakpoints: {
-                    640: {
+
+                
+                    768: {
                         perPage: 4,
                         gap    : '.7rem',
+                        // height : '6rem',
+                    },
+                    576: {
+                        perPage: 3,
+                        gap    : '.7rem',
+                        // pagination: true
                         // height : '6rem',
                     },
                     480: {
@@ -231,6 +107,12 @@ for ( var i = 0; i < elms.length; i++ ) {
                         // pagination: true
                         // height : '6rem',
                     },
+                    320: {
+                        perPage: 2,
+                        // gap    : '.7rem',
+                        // pagination: true
+                        // height : '6rem',
+                    }
             },
 
         }
